b2be26d06adf489e24e8d3807349b4d50d2cc56e
Camden Phalen <camdenphalen@gmail.com>
2018-03-14 21:37:45 -0400

    improved color algorithm

--- src/reducers/indico.js
+++ src/reducers/indico.js
@@ -4,9 +4,30 @@ import buildResults from "../utils/buildResults";
 
 const initialState = {
   results: {
-    emotion: {},
-    personality: {},
-    topics: {}
+    sentiment: 0,
+    emotion: {
+      anger: 0,
+      fear: 0,
+      joy: 0,
+      sadness: 0,
+      surprise: 0
+    },
+    keywords: [],
+    personality: {
+      agreeableness: 0,
+      conscientiousness: 0,
+      extraversion: 0,
+      openness: 0
+    },
+    topics: {
+      health: 0,
+      jobs: 0,
+      nostalgia: 0,
+      relationships: 0,
+      romance: 0,
+      school: 0,
+      weather: 0
+    }
   },
   status: RequestStatus.UNITIALIZED
 };
